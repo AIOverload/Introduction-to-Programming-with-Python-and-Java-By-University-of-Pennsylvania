a13290e099c4dd7ebd969fa8f09d9e9587a68f41
Warren Gipson <98239043+warren2391@users.noreply.github.com>
2023-08-20 17:21:16 -0700

    add file
    Updated Version of this code

--- Numerical grade to letter grade.py
+++ Numerical grade to letter grade.py
@@ -1,17 +1,21 @@
-#get user input of a numerical grade
-grade = input("Enter your grade: " )
+def numeric_to_letter_grade():
+    #get user input of a numerical grade
+    grade = input("Enter your grade: " )
 
-#cast grade to an int
-grade = int(grade)
+    #cast grade to an int
+    grade = int(grade)
 
-#test the range of the number and print the appropriate letter grade
-if grade >= 90:
-    print('A')
-elif grade >= 80:
-    print('B')
-elif grade >= 70:
-    print('C')
-elif grade >= 60:
-    print('D')
-else:
-    print('F')
+    #test the range of the number and print the appropriate letter grade
+    if grade >= 90:
+        print('A')
+    elif grade >= 80:
+        print('B')
+    elif grade >= 70:
+        print('C')
+    elif grade >= 60:
+        print('D')
+    else:
+        print('F')
+
+#call the numeric_to_letter_grade function defined above
+numeric_to_letter_grade()
